92ae6cda30c628f0237510811e3b6ccefa2a375f
Tran Viet Cuong <rinjin2004@gmail.com>
2025-09-22 12:10:33 +0700

    feat: add missed automapper profile for entity

---
+++ Parentee_BE.DAL/Mappers/DiaperChangeMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Parentee_BE.DAL.Data.RequestDTO.DiaperChange;
+using Parentee_BE.DAL.Data.ResponseDTO.DiaperChange;
+
+namespace Parentee_BE.DAL.Mappers;
+
+public class DiaperChangeMapper : Profile
+{
+    public DiaperChangeMapper()
+    {
+        // Request -> Entity
+        CreateMap<CreateDiaperChangeRequest, DiaperChangeMapper>();
+        CreateMap<UpdateDiaperChangeRequest, DiaperChangeMapper>();
+
+        // Entity -> Response
+        CreateMap<DiaperChangeMapper, GetDiaperChangeResponse>();
+    }
+}
--- Parentee_BE.DAL/Mappers/FeedingMapper.cs
+++ Parentee_BE.DAL/Mappers/FeedingMapper.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Parentee_BE.DAL.Data.Entities;
 using Parentee_BE.DAL.Data.RequestDTO;
+using Parentee_BE.DAL.Data.RequestDTO.Feedings;
 using Parentee_BE.DAL.Data.ResponseDTO.Feedings;
 
 namespace Parentee_BE.DAL.Mappers;
@@ -11,6 +12,7 @@ public class FeedingMapper : Profile
     {
         // Request -> Entity
         CreateMap<CreateFeedingRequest, FeedingEntity>();
+        CreateMap<UpdateFeedingRequest, FeedingEntity>();
 
         // Entity -> Response
         CreateMap<FeedingEntity, GetFeedingResponse>();
