3a16803e200093de2ef1d78715512809ac2e1418
Tran Viet Cuong <rinjin2004@gmail.com>
2025-11-02 14:20:22 +0700

    feat: swagger for product

--- Parentee_BE.API/Program.cs
+++ Parentee_BE.API/Program.cs
@@ -310,9 +310,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+   
 }
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseMiddleware<ExceptionHandlerMiddleware>();
 
